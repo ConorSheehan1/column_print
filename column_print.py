6bbb0dab623a55c8d2d6a941192d5d5b33bd58fd
conor <conor.sheehan@live.ie>
2017-07-23 00:59:32 +0100

    loop edge cases handled

--- column_print.py
+++ column_print.py
@@ -1,21 +1,24 @@
-def column_print(strings, max_len=32):
+def column_print(strings, page_width=32, tab_width=4):
     ncols = len(strings)
     #add space for tabs
-    max_len -= (ncols-1)*4
-    width = max_len//ncols
+    page_width -= (ncols - 1) * tab_width
+    width = page_width // ncols
 
-    while strings:
+    # while there is data left to print
+    while not all(s == "" for s in strings):
         for i, s in enumerate(strings):
-            if i==0:
-                print()
-            to_print = " "*width if s=="" else s[:width]
-            if len(to_print) < width:
-                to_print+=(" "*(width-len(to_print)))
-            tab = "" if i==len(strings)-1 else " " *4
+            # go to a new line for each column
+            if i == 0: print()
+            # if the column data doesn't exist create it using appropraite space
+            to_print = " " * width if s == "" else s[:width]
+            # if the column data does exist but is too short, 
+            # add space to keep printing uniform
+            if len(to_print) < width: to_print += (" "*(width-len(to_print)))
+            # add tab space to the right of every column except the last
+            tab = "" if i==len(strings)-1 else " " *tab_width
+            # remove printed data from list of strings 
             strings[i] = s[width:]
             print(to_print+tab, end="")
-        if all(s=="" for s in strings):
-            break
     print()
         
             
